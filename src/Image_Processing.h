51cde1bb256c60694ab1912fb080f5129d9a8fbb
gccAlexandruDarie <101355884+gccAlexandruDarie@users.noreply.github.com>
2024-01-10 21:18:35 +0200

    Update Image_Processing.h

--- src/Image_Processing.h
+++ src/Image_Processing.h
@@ -51,9 +51,8 @@ BYTE*   getHistogram(BYTE* src, BYTE*& dest, int w, int h);
 BYTE*   normalizeImage(BYTE* src, BYTE*& dest, int w, int h);
 //BYTE*   DeleteSmallArtifacts(BYTE* src, BYTE*& dest, int w, int h, unsigned int Iterations); // Better version below?
 BYTE*   DeleteSmallArtifacts(BYTE* src, BYTE*& dest, int w, int h, unsigned int IterationsErode, unsigned int IterationDilate);
-void    processBinaryImage(BYTE* binary_src, BYTE*& dest, int w, int h, double min_contour_area = 100.0);
-//BYTE* ImageProcessing::RegionGrowing(BYTE* src, BYTE*& matDEST, int threshold, int x, int y);
-
+double		processBinaryImage(const cv::Mat& binary_src, cv::Mat& dest, double min_contour_area = 100.0, double max_contour_area=DBL_MAX);
+bool		rayTraceCentroids(const cv::Mat& matSRC, const cv::Point2d& centroid, const unsigned int N_Rays, double acceptable_deviation = 1e-6);
 //====================================================================================================
 //Global variables
 //====================================================================================================
